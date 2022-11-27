60b5fd1987d67a428b6c178e6ced30990a308c51
indymnv <indy.mnv@gmail.com>
2022-11-27 21:47:58 +0900

    adding base for documentation

--- src/functions.jl
+++ src/functions.jl
@@ -1,3 +1,12 @@
+# Example of function inside ./src/Hikari.jl
+"""
+    greet_your_package_name()
+This is an example of Docstring. This function say Hello
+Hikari
+```julia
+println("Hello Hikari") 
+```
+"""
 function greet_your_package_name()
     return "Hello Hikari"
 end
